520ce13dec64295d1c007593ab4311e3eaa1e5fe
Jonathan Serafini <jonathan.serafini@gmail.com>
2018-06-15 15:34:26 +0200

    add routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'quizzes/new'
+  get 'quizzes/create'
+  get 'quizzes/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'quizz', to: 'pages#quizz'
+
+  resources :quizzes, only: [:new, :create, :show]
 end
