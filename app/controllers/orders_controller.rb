954e8fc01670e8f6c06d9be1b513de9905133605
Jonathan Serafini <jonathan.serafini@gmail.com>
2018-07-24 20:31:44 +0200

    add landing

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,21 +3,26 @@ class OrdersController < ApplicationController
   end
 
   def create
-    customer = Stripe::Customer.retrieve(stripe_id)
+    @book = Book.find(params[:book_id])
+    customer = Stripe::Customer.retrieve(current_user.stripe_id)
+    customer.sources.create( source: params[:stripeToken] )
 
     charge = Stripe::Charge.create(
       customer:     customer.id,
-      source:       params[:stripeToken],
-      amount:       @order.price_cents,
-      description:  "Payment for book #{@book.title} for order #{@order.id}",
-      currency:     @order.price.currency
+      amount:       @book.price_cents,
+      description:  "Payment for book #{@book.title}",
+      currency:     @book.price.currency
     )
 
-    @order.update(payment: charge.to_json)
-    redirect_to order_path(@order)
+    @order = Order.create(  payment: charge.to_json,
+                    user: current_user,
+                    book: @book,
+                    amount: @book.price
+                  )
+    redirect_to @order
 
   rescue Stripe::CardError => e
     flash[:alert] = e.message
-    redirect_to new_order_payment_path(@order)
+    redirect_to @book
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :survey]
 
   def home
+    render :layout => false
   end
 
   def survey
