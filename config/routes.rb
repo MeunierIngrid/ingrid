2f8e7b210142f7bafd1dc5397e4567f1f4922505
Jonathan Serafini <jonathan.serafini@gmail.com>
2018-09-09 23:05:29 +0200

    add reviews to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'books/index'
-  get 'books/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
