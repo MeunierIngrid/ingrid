7edfa090b0186a6fa8c5891d3c14075e630b0115
Jonathan Serafini <jonathan.serafini@gmail.com>
2018-09-09 18:15:43 +0200

    texts books index

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -1,15 +1,28 @@
 ActiveAdmin.register Book do
 
-  permit_params :title, :token, :cover
+  permit_params :title, :token, :cover, :price
 
   form do |f|
     f.inputs do
       f.input :title
       f.input :description
+      f.input :price
       # f.input :token
       f.input :cover, as: :file
       f.button :submit
     end
   end
+
+  show do
+    attributes_table do
+      row :title
+      row :description
+      row :price
+      row :cover do |book|
+        image_tag url_for(book.cover)
+      end
+    end
+  end
+
 
 end
