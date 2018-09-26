737304f6fcb12a935a73a422d49a7f43676ae724
Jonathan Serafini <jonathan.serafini@gmail.com>
2018-09-26 12:51:58 +0200

    min fix front to survey new view

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,11 @@
 class Answer < ApplicationRecord
   belongs_to :survey, inverse_of: :answers
   belongs_to :question
+
+  def self.weighted_average
+    all.inject do |sum, answer|
+      sum + answer.score * answer.question.
+
+    end
+  end
 end
