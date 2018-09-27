f4307599708d0db73bf3cab8013096b0c3feab83
Jonathan Serafini <jonathan.serafini@gmail.com>
2018-09-27 09:14:05 +0200

    clean js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,38 @@
+// ############ page#home
+$(document).ready(function(){
+  $('.hover').hover(function(){
+    $(this).addClass('flip');
+  },function(){
+    $(this).removeClass('flip');
+  });
+});
+
+// ############ survey#new
+
+// ajax radio_buton
+const sessionButton = document.querySelector("#update-session");
+const radios = document.querySelectorAll(".number");
+radios.forEach((radio)=>{
+  radio.addEventListener("click", (e)=>{
+    e.preventDefault();
+    sessionButton.href = `${sessionButton.innerText}?question_id=${e.currentTarget.dataset.qid}&score=${e.currentTarget.innerText}`;
+    sessionButton.click();
+  });
+});
+
+// sroll progress bar
+$(document).on('scroll',function(){
+  if ($(document).scrollTop() > 330) {
+   $('#total-scores').addClass('fixed')
+   $('#wrapper-after-progress-bar').addClass('margin-for-progress-bar-fixed')
+  } else {
+   $('#total-scores').removeClass('fixed')
+   $('#wrapper-after-progress-bar').removeClass('margin-for-progress-bar-fixed')
+  }
+});
+
+// ############ survey#show
+
 const run = () => {
   const circles = document.querySelectorAll('.js-circle')
   circles.forEach((circle) => {
@@ -7,5 +42,4 @@ const run = () => {
     circle.style.strokeDashoffset = Math.round(1100 - diameter * val / 100)
   })
 }
-
 document.addEventListener('DOMContentLoaded', () => setTimeout(run, 10))
