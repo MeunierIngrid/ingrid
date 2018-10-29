082ac0a805324375e5c0407b346aa4928c1fe493
Jonathan Serafini <jonathan.serafini@gmail.com>
2018-10-29 12:49:23 +0100

    add generator of eboo slug

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,9 +5,15 @@ class Book < ApplicationRecord
   has_one_attached :pdf
   monetize :price_cents
   has_many :orders
-  validates :slug, uniqueness: true
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  before_create :add_slug
 
   def visible?
     visible
+  end
+
+  def add_slug
+    self.slug = title.parameterize
   end
 end
