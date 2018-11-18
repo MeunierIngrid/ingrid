fb5756aebb5f8255a1896a2d587aa4a006e23a9a
Jonathan Serafini <jonathan.serafini@gmail.com>
2018-11-18 23:46:13 +0100

    add visible field

--- app/models/questions_category.rb
+++ app/models/questions_category.rb
@@ -6,10 +6,14 @@ class QuestionsCategory < ApplicationRecord
     where(visible: true)
   end
 
+  def visible_questions
+    questions.visibles
+  end
+
   def self.set_total_scores(answers_attrs)
     session_qids = answers_attrs.keys.map(&:to_i)
     all.map do |questions_category|
-      category_qids = questions_category.questions.map(&:id)
+      category_qids = questions_category.questions.visibles.map(&:id)
       total_score = (category_qids & session_qids).size * 100 / category_qids.size
       [ questions_category.title, total_score ]
     end.to_h
