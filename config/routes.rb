e21f13db5250e3b646523ca15b36c74c5adf433d
Sylvain Galtier <sylvaingaltier@gmail.com>
2019-02-15 18:30:14 +0100

    bootstrap class on homepage links and routes rescues deleted

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self) rescue ActiveAdmin::DatabaseHitDuringLoad
+  ActiveAdmin.routes(self)
 
   require "sidekiq/web"
   authenticate :user, lambda { |u| u.admin } do
